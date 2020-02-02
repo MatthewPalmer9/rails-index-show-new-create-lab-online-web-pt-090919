a7ae7bb6bd5cebac32c16ef913f6334d94f616a7
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2020-02-02 05:36:46 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -13,6 +13,9 @@ class CouponsController < ApplicationController
 
   def create
     @coupon = Coupon.new
-    @coupon.coupon_code = 
+    @coupon.coupon_code = params[:coupon_code]
+    @coupon.store = params[:store]
+    @coupon.save
+    redirect_to coupon
   end
 end
